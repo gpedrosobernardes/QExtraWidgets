7eb5b30a8dacb7f970aa08c1f9136223268a6f64
Gustavo Pedroso Bernardes <gpedrosobernardes@gmail.com>
2026-05-31 16:44:59 -0300

    refactor: removed unnecessary new instance of QPersistentModelIndex

--- source/qextrawidgets/widgets/delegates/grid_icon_delegate.py
+++ source/qextrawidgets/widgets/delegates/grid_icon_delegate.py
@@ -155,16 +155,14 @@ class QGridIconDelegate(QStyledItemDelegate):
         # --- Lazy Loading Logic ---
         # If no valid data is found, trigger the signal
         is_data_valid = False
-        if item_data is not None:
+
+        if index not in self._requested_indices:
+            self._requested_indices.add(index)
+            self.requestImage.emit(index)
+
+        elif item_data is not None:
             if isinstance(item_data, (QIcon, QPixmap, QImage)) and not item_data.isNull():
                 is_data_valid = True
-
-        # Check if we already requested this index to avoid spamming the signal in the paint loop
-        p_index = QPersistentModelIndex(index)
-        if p_index not in self._requested_indices:
-            self._requested_indices.add(p_index)
-            self.requestImage.emit(p_index)
-            is_data_valid = False
 
         if not is_data_valid:
             # Optional: Draw a placeholder (e.g., a simple loading circle or gray box)
